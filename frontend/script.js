087b56d86bddd9f6f311f4da0da97f46b4c24f77
Vansh Motla <vansh.motla07@gmail.com>
2026-06-09 13:21:52 +0530

    Connect frontend to deployed API

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ let heatLayer = null;
 
 // 🔥 Core prediction call
 async function fetchPrediction(lat, lon) {
-  const res = await fetch("http://127.0.0.1:5000/predict", {
+  const res = await fetch("https://disaster-project-6wtu.onrender.com/predict", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({ lat, lon })
